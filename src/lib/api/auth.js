4cd94cb7f004b8b5f54f36fbc9ca59fad6d487c3
TahirReyaz <tahirreyaz5@gmail.com>
2024-11-30 12:28:15 +0530

    Migrated api code to /api

---
+++ src/lib/api/auth.js
@@ -0,0 +1,28 @@
+import apiClient from ".";
+
+export const loginApi = async ({ username, password }) => {
+  try {
+    const response = await apiClient.post("/auth/login", {
+      username,
+      password,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signup = async ({ username, password, email }) => {
+  try {
+    const response = await apiClient.post("/auth/signup", {
+      username,
+      password,
+      email,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
